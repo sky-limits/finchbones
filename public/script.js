789a9524377896d3ff1e003394b353d5816002bf
faunidsdev <lilybean4868@outlook.com>
2025-01-07 00:49:28 -0500

    please...header

--- public/script.js
+++ public/script.js
@@ -66,10 +66,10 @@ if ( url.includes("posts/") ) {
 }
 
 //Generate the Header HTML, a series of list items containing links.
-let headerHTML = '<ul> <li><a href="' + relativePath + '/index-1.html">Blog Home</a></li>' +
-'<li><a href="' + relativePath + '/index.html">Main Site</a></li>' +
-'<li><a href="' + relativePath + '/archive.html">Archive</a></li>' +
-'<li><a href="' + relativePath + '/about.html">About</a></li> </ul>';
+let headerHTML = '<ul> <li><a href="/index-1.html">Blog Home</a></li>' +
+'<li><a href="/index.html">Main Site</a></li>' +
+'<li><a href="/archive.html">Archive>/a></li>' +
+'<li><a href="/about.html">About</a></li> </ul>';
 
 //Generate the Footer HTML, which uses the variables defined in the BASIC INFO section above to list info about the site.
 //Note: feel free to remove the references to Zonelets and Neocities! Just be careful not to delete any necessary HTML closing tags or other syntax.
