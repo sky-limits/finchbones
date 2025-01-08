8a4467a115c7e6bbc85bd54abf29e8d15d0beb45
faunidsdev <lilybean4868@outlook.com>
2025-01-08 00:08:43 -0500

    new post please post challenge

--- public/script.js
+++ public/script.js
@@ -38,8 +38,8 @@ let postsArray = [
 [ "posts/2024-12-26-it lives!.html" ],
 [ "posts/2025-01-02-general-thoughts-on-life.html" ],
 [ "posts/2025-01-07-red-dead-redemption-2-act-I-part-I.html" ], 
-[ "posts/2025-01-07-about-that-time-again.html"],
-[ ""]
+[ "posts/2025-01-08-about-that-time-again.html" ],
+[ "" ]
 ];
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
