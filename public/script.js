eb785f9d87a1778eb6abfe6a3e00fc2d2e28a3d9
faunidsdev <lilybean4868@outlook.com>
2025-01-09 10:31:15 -0500

    1/9 new post

--- public/script.js
+++ public/script.js
@@ -39,7 +39,8 @@ let postsArray = [
 [ "posts/2025-01-02-general-thoughts-on-life.html" ],
 [ "posts/2025-01-07-red-dead-redemption-2-act-I-part-I.html" ], 
 [ "posts/2025-01-07-about-that-time-again.html" ],
-[ "posts/2025-01-08-guilt.html" ]
+[ "posts/2025-01-08-guilt.html" ],
+[ "posts/2025-01-09-new-years-resolutions.html" ]
 ];
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
