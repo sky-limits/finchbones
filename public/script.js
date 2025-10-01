cbcd9e215aa899780e761fa91b8bea23b6c097d0
faunidsdev <lilybean4868@outlook.com>
2025-10-01 15:58:56 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -40,7 +40,8 @@ let postsArray = [
 [ "posts/2025-01-07-red-dead-redemption-2-act-I-part-I.html" ], 
 [ "posts/2025-01-07-about-that-time-again.html" ],
 [ "posts/2025-01-08-guilt.html" ],
-[ "posts/2025-01-09-new-years-resolutions.html" ]
+[ "posts/2025-01-09-new-years-resolutions.html" ],
+[ "posts/2025-10-01-update-time.html" ]
 ];
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
